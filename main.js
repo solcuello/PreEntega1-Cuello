ab1371fc49143b25c22279921b8ba79a3cb12747
maria sol cuello <msc.solcuello@gmail.com>
2023-07-31 00:14:28 -0300

    subida inicial del index

---
+++ main.js
@@ -0,0 +1,65 @@
+//Comentario de linea
+
+/* 
+comentario
+de 
+bloque
+*/
+
+/*  comentario de bloque ALT+SHIFT+A*/
+
+/* /* /* variable de tipo number -string */
+/* let edad = 15;
+let nombre = "andres";
+
+/* constantes 
+const CONSTANTE = 5;
+/* 
+let numeroUno = 5; 
+let numeroDos = 10;
+
+let resultadoSuma = numeroUno + numeroDos;
+let resultadoResta = numeroUno - numeroDos;
+let resultadoMultiplicacion = numeroUno * numeroDos;
+let resultadoDivision = numeroUno / numeroDos;
+
+
+console.log("resultado suma", resultadoSuma);
+console.log("resultado resta", resultadoResta);
+console.log("resultado multiplicacion", resultadoMultiplicacion);
+console.log("resultado division", resultadoDivision); */
+
+/* 
+let saludo = "Hola";
+let nombre = "Camila";
+let saludoCompleto = saludo + " " + nombre;
+
+console.log(saludoCompleto);
+ */
+
+/* prompt es para introducir la info 
+Alert es para que aparezca la info 
+ */
+/* let numero = prompt("Ingrese un numero");
+alert(numero + 10);
+ */
+/* let numero = 8;
+let nombre = "andres";
+
+console.log(typeof numero);
+console.log(typeof nombre); */
+/* 
+con PROMT se pide los datos
+con ALERT se muestran  */
+
+/* 
+let saludo = "Hola";
+let nombre = "Cecilia";
+let saludoCompleto =prompt(saludo + " " + nombre);
+
+console.log(saludoCompleto); */
+
+
+let nombre = prompt("Ingrese su nombre");
+alert("hola"+ " " + nombre);
+
