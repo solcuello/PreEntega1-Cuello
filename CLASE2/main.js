5727d9dfe58dc1097d7d41037edc956bd35e56a3
maria sol cuello <msc.solcuello@gmail.com>
2023-07-31 23:14:56 -0300

    subida clase 2

---
+++ CLASE2/main.js
@@ -0,0 +1,32 @@
+/* estructura del if
+
+if(condicion)
+codigo a ejecturar cuando la condicion es verdadera*/
+
+/* let edad = 16;
+
+if(true < 18){
+    console.log("No puedes entrar a la fiesta");
+} */
+
+if(false){
+    console.log("entramos");
+}
+console.log("Finalizamos");
+
+/* estructura del if else
+
+if(condicion){
+    codigo a ejecutar en casi de que la condicion sea verdadera
+}else (significa que si lo de arriba no es así se ejecuta lo de abajo)
+    {
+    codigo a ejecutar en caso de que la condicion no sea verdadera }
+*/
+
+led edad = parseInt(prompt("ingrese su edad"));
+
+if(Edad < 18){
+    alert("no puedes entrar a la fiesta");
+}else{
+    alert("puedes entrar a la fiesta");
+}
